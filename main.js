aa6a752d30d9b6a11923d7c5ee289256bc5becfa
hani <hani_11_123@hotmail.com>
2023-04-10 16:43:41 +0100

    Sort the array code wars

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /* 
 👉 Write your kata here!
-- Need to list the arrays from highest number to lowest number 
 // Create a function that takes a random array and returns a sorted array from highest to lowest number
 */ 
 
@@ -8,4 +7,8 @@
 
 //👉 Write the function your CodeWarriors will start with below here:
 
-function sortArray(array) {}
+function sortArray(array) {
+    return array
+}
+
+
