28392d83d89772a79393db68f8048ec7168374cc
hani <hani_11_123@hotmail.com>
2023-04-10 16:38:56 +0100

    completed code wars task

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 /* 
 👉 Write your kata here!
-*/
+- Need to list the arrays from highest number to lowest number 
+// Create a function that takes a random array and returns a sorted array from highest to lowest number
+*/ 
+
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function sortArray(array) {}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,29 @@
 //👉 Write your tests below here:
+
+import {sortArray} from './main.js';
+import {test,expect} from '@jest/globals';
+
+
+test('sorting Array', function () {
+    let received = [300, 78, 54, 12, 5, 2];
+    let expected = [300, 78, 54, 12, 5, 2];
+    expect(received).toEqual(expected);
+})
+
+test ('sorting Array with negative numbers ', function () {
+    let received = [-300, -78, -54, -12, -5, -2];
+    let expected = [-300, -78, -54, -12, -5, -2];
+    expect(received).toEqual(expected);
+})
+
+test ('sorting Array with negative and positive numbers ', function () {
+    let received = [-300, -78, -54, 12, 5, 2];
+    let expected = [-300, -78, -54, 12, 5, 2];
+    expect(received).toEqual(expected);
+})
+
+test ('sorting Array with no numbers ', function () {
+    let received = [];
+    let expected = [];
+    expect(received).toEqual(expected);
+})
